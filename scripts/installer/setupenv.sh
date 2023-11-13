f8b121ffdea5889eac082251c15c21e63322d282
Muffaddal Kalla <muf@bqa.la>
2023-11-13 22:09:21 +0000

    Updates

--- scripts/installer/setupenv.sh
+++ scripts/installer/setupenv.sh
@@ -20,3 +20,13 @@ cd /opt/paperless
 git config --global --add safe.directory /opt/paperless
 git config --global --add user.name "Muffaddal Kalla"
 git config --global --add user.email "muf@bqa.la"
+
+ln -s /opt/paperless/scripts/m-webserver.service /etc/systemd/system/m-webserver.service
+ln -s /opt/paperless/scripts/m-webserver.socket /etc/systemd/system/m-webserver.socket
+ln -s /opt/paperless/scripts/m-consumer.service /etc/systemd/system/m-consumer.service
+ln -s /opt/paperless/scripts/m-queue.service /etc/systemd/system/m-queue.service
+ln -s /opt/paperless/scripts/m-scheduler.service /etc/systemd/system/m-scheduler.service
+
+systemctl enable m-webserver m-consumer m-queue m-scheduler
+systemctl start m-webserver m-consumer m-queue m-scheduler
+
