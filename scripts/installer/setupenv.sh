0b98a2b3bd28346bf97fb4d8f1a51ed570cfbf86
Muffaddal Kalla <muf@bqa.la>
2024-06-27 20:39:26 +0000

    updates

--- scripts/installer/setupenv.sh
+++ scripts/installer/setupenv.sh
@@ -1,30 +1,33 @@
-cd /opt/paperless
-#cp paperless.conf.example paperless.conf
-pyenv global 3.11.6
-pyenv local 3.11.6
+su - paperless <<EOF
 
-pipenv install
-pipenv requirements > req.txt
-pip install -r req.txt
-sudo -Hu paperless pip3 install -r req.txt --break-system-packages
-
-sudo -Hu paperless python3 -m nltk.downloader popular
+pipenv requirements >> req.txt
+pip install --upgrade pip
+pip install --upgrade setuptools
+pip install -r req.txt --force
+python3 -m nltk.downloader popular
 
 cd /opt/paperless/src
-sudo -Hu paperless python3 manage.py migrate
-#sudo -Hu paperless python3 manage.py createsuperuser
+python3 manage.py migrate
+python3 manage.py createsuperuser
+
 
 cd /opt/paperless/src-ui
-npm i -g npm@latest --force
-npm install -g @angular/cli
+
+curl -fsSL https://deb.nodesource.com/setup_22.x -o nodesource_setup.sh
+sudo -E bash nodesource_setup.sh
+sudo apt-get install -y nodejs
+
+sudo npm i -g npm@latest --force
+sudo npm install -g @angular/cli
 npm install --force
 ng build --configuration production
-
 
 cd /opt/paperless
 git config --global --add safe.directory /opt/paperless
 git config --global --add user.name "Muffaddal Kalla"
 git config --global --add user.email "muf@bqa.la"
+
+EOF
 
 ln -s /opt/paperless/scripts/m-webserver.service /etc/systemd/system/m-webserver.service
 ln -s /opt/paperless/scripts/m-webserver.socket /etc/systemd/system/m-webserver.socket
@@ -35,4 +38,3 @@ ln -s /opt/paperless/scripts/m-scheduler.service /etc/systemd/system/m-scheduler
 systemctl daemon-reload
 systemctl enable m-webserver m-consumer m-queue m-scheduler
 systemctl start m-webserver m-consumer m-queue m-scheduler
-
