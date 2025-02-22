ee725b0c00610092e8b89bfe2c7d2835aa8842e5
Muffaddal Kalla <muf@bqa.la>
2025-02-22 15:55:29 +0000

    updates

--- src-ui/src/app/hooks/hook.component.ts
+++ src-ui/src/app/hooks/hook.component.ts
@@ -2,58 +2,6 @@ import { Component, Input, OnInit, ContentChild, TemplateRef, AfterViewInit, Dir
 import { Hooks, HookService } from './hook.service'; // Assume HookService triggers hooks.
 import { CommonModule } from '@angular/common';
 
-// @Directive({
-//   selector: '[pngxHook]',
-// })
-// export class HookComponent implements OnInit {
-//   @Input() hookName: string = '';  // Name of the hook
-//   @Input() hookParam: any = {};    // Data to pass to the hook
-
-//   @Input() set pngxHook(output: string) {
-
-//     this.outputs.forEach(item => {
-//       console.log(item);
-//       this.viewContainer.createEmbeddedView(this.templateRef, {
-//         [output]: item,
-//         $implicit: item  // Assign 'output' to '$implicit'
-//       });
-//     })
-
-//   }
-
-//   hookResults: { registrar: string, output: any[] }[] = [];  // Results from the hook  
-
-//   public get outputs(): any[] {
-//     return this.hookResults
-//       .map(result => result.output) // Extract 'output' from each object
-//       .flat()
-//   }
-
-
-//   // // Content projection with TemplateRef
-//   // @ContentChild('hookTemplate') hookTemplate: TemplateRef<any> | null = null;
-
-//   constructor(
-//     private hookService: HookService,
-//     private templateRef: TemplateRef<any>,  // Template reference
-//     private viewContainer: ViewContainerRef  // View container to render the template
-//   ) { }
-
-
-//   ngOnInit(): void {
-//     if (this.hookName && this.hookParam) {
-//       this.triggerHook();
-//     }
-//   }
-
-//   // Method to trigger the hook and get results
-//   triggerHook(): void {
-//     this.hookResults = this.hookService.triggerHook(this.hookName, this.hookParam);
-//   }
-// }
-
-
-
 @Directive({
   selector: '[pngxHook]',
 })
@@ -117,10 +65,5 @@ export class HookComponent implements OnChanges {
     //     });
     //   });
     // }
-  }
-
-
-  triggerHook(): void {
-    // this.hookResults = this.hookService.triggerHook(this.pngxHook.name || this.pngxHook , this.pngxHook.param);
   }
 }
